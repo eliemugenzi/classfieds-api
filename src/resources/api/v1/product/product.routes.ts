2e56c7caba93649690d39f10893261dcadddb014
Elie Mugenzi <eliemugenzi@gmail.com>
2021-07-06 11:12:43 +0200

    Fix my products

--- src/resources/api/v1/product/product.routes.ts
+++ src/resources/api/v1/product/product.routes.ts
@@ -6,8 +6,9 @@ import { createOneRule } from './product.validator';
 const router = Router();
 
 router.post('/', checkAuth({ roles: ['seller'] }), createOneRule, createOne);
+router.get('/my_products', checkAuth({ roles: ['seller'] }), getMine);
+
 router.get('/:id', checkAuth(), checkProduct, getOne);
 router.get('/', checkAuth(), getMany);
-router.get('/my_products', checkAuth({ roles: ['seller'] }), getMine);
 
 export default router;
