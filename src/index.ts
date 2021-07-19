716a5c1cabd4dc493456bcbb6c24b00adda2c04a
Elie Mugenzi <eliemugenzi@gmail.com>
2021-07-19 17:50:33 +0200

    Improve DevOps

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import app from './app';
 
 const { PORT = 9000 } = process.env;
 
+const serverHost = '0.0.0.0';
+
 // Start server
-app.listen(PORT, () =>
+app.listen(PORT as number, serverHost, () =>
   console.log(`\nServer is running on http://localhost:${PORT}/api/v1/docs ...\n`),
 );
 
